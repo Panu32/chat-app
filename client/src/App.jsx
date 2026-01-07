b30a221b4e7dd7566a39dc63cbfe79516bda50f2
Pranav Vaidya <pranavvaidya2005@gmail.com>
2026-01-07 16:07:01 +0530

    server and client  updated for vercel

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,29 @@
-import  { useContext } from 'react'
+import { useContext } from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
 import ProfilePage from './pages/ProfilePage'
-import {Toaster} from "react-hot-toast"
+import { Toaster } from "react-hot-toast"
 import { AuthContext } from '../context/AuthContextObject'
 
 // Step 1: Import the image as a variable
 import bgImage from './assets/bgImage.svg'
 
 const App = () => {
-  const {authUser}  = useContext(AuthContext);
+  const { authUser } = useContext(AuthContext);
   return (
     // Step 2: Use an inline style for the background image
     // The build tool will replace `bgImage` with the correct final URL.
     <div
-      style={{ backgroundImage: `url(${bgImage})` }}
+      style={{ backgroundImage: `url(/bgImage.png)` }}
       className="bg-contain"
     >
-      <Toaster/>
+
+      <Toaster />
       <Routes>
-        <Route path='/' element={authUser ? <HomePage /> : <Navigate to="/login"/>} />
-        <Route path='/login' element={!authUser ? <LoginPage /> :<Navigate to="/" />} />
-        <Route path='/profile' element={authUser ? <ProfilePage /> : <Navigate to="/login"/>} />
+        <Route path='/' element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+        <Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
+        <Route path='/profile' element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
       </Routes>
     </div>
   )
